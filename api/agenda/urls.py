575bf3cf5f9371d7793ec9f8b8c4ae676a007c29
claytonsilva007 <claytonsilva007@gmail.com>
2025-03-13 23:06:44 -0300

    Ajustes nas urls da api.

--- api/agenda/urls.py
+++ api/agenda/urls.py
@@ -3,7 +3,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from agenda import views
 
 urlpatterns = [
-    path('agenda/', views.AgendaList.as_view()),
+    path('', views.AgendaList.as_view()),
     path('agenda/<int:pk>/', views.AgendaDetail.as_view()),
 ]
 
--- api/core/urls.py
+++ api/core/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('agenda.urls')),
+    path('api/', include('agenda.urls')),
 ]
